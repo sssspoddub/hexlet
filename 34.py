55f454878bcd6f0040f21ea653f70e21c2f35151
sssspoddub <spoddub@yahoo.com>
2025-02-14 12:19:01 +0300

    add 4 completed lessons

---
+++ 34.py
@@ -0,0 +1,6 @@
+def print_numbers(number: int):
+    i = number
+    while i > 0:
+        print(i)
+        i -= 1
+    print('finished!')
---
+++ 36.py
@@ -0,0 +1,2 @@
+def get_substr(string: str, length: int) -> str:
+    return string[:length]
